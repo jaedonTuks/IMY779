19b5dd42497dbc650c8179267f90010537d24f30
Jaedon Heger <jaedon.heger@gmail.com>
2022-09-19 21:29:54 +0200

    Adding hand tracked velocity based gun trigger
    - adding a script to calculate velocity of right hand to be used as a trigger
    
    Issue: #3

---
+++ Assets/Scripts/VelocityBasedGunController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityBasedGunController : MonoBehaviour
+{
+    [Header("Velocity Settings")]
+    public Vector3 worldVelocity;
+    public Vector3 localVelocity;
+
+    public Vector3 prevPosition;
+
+    [Header("Gun settings")]
+    public float movementThreshold = -0.03f;
+    public GunController gunController;
+
+    public int inFramesBetweenShot = 30;
+
+    public int framesSinceLastShot = -1;
+
+
+    private void Start()
+    {
+        prevPosition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        worldVelocity = (transform.position - prevPosition) / (Time.deltaTime * inFramesBetweenShot);
+        localVelocity = transform.InverseTransformDirection(worldVelocity);
+        prevPosition = transform.position;
+
+        checkAndTriggerGunShot();
+    }
+
+    private void checkAndTriggerGunShot()
+    {
+        if(framesSinceLastShot == -1 && localVelocity.z < movementThreshold)
+        {
+            Debug.Log("Movement triggered shot");
+            gunController.shootGun();
+            framesSinceLastShot = 0;
+        }
+
+        if(framesSinceLastShot > inFramesBetweenShot)
+        {
+            framesSinceLastShot = -1;
+        } else if (framesSinceLastShot != -1)
+        {
+            framesSinceLastShot++;
+        }
+        
+    }
+}

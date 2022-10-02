fb2fef63d482ca44beba883833378d595a75ec7c
Jaedon Heger <jaedon.heger@gmail.com>
2022-10-02 15:56:12 +0200

    Creating basic spawn controller
    - adding basic spawn controller which instantiates a Target prefab
    
    Todo:
    - implement timeout for no longer spawning
    - implement random position spawning
    - implement speed increase
    
    Issue: #16

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public Transform leftSpawnPoint;
+    public Transform middleSpawnPoint;
+    public Transform rightSpawnPoint;
+    public GameObject targetPrefab;
+    public Dictionary<SpawnPoint, Transform> spawnPointMap;
+
+
+
+
+    private void Start()
+    {
+        spawnPointMap = new Dictionary<SpawnPoint, Transform>()
+        {
+            { SpawnPoint.Left, leftSpawnPoint },
+            { SpawnPoint.Middle, middleSpawnPoint },
+            { SpawnPoint.Right, rightSpawnPoint }
+        };
+    }
+
+    public void SpawnNewTarget(SpawnPoint? spawn)
+    {
+        Transform spawnTransform = GetSpawnPointTransform(spawn);
+
+
+        Debug.Log("Spawning new target");
+        Instantiate(targetPrefab, spawnTransform.position, spawnTransform.rotation);
+    }
+
+
+    public Transform GetSpawnPointTransform(SpawnPoint? spawn)
+    {
+        Transform returnTransform;
+        return spawnPointMap.TryGetValue(spawn ?? SpawnPoint.Middle , out returnTransform) ? returnTransform : middleSpawnPoint;
+    }
+}
---
+++ Assets/Scripts/models/SpawnPoint.cs
@@ -0,0 +1,6 @@
+public enum  SpawnPoint
+{
+    Left,
+    Middle,
+    Right
+}
